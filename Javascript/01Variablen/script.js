c6cb96698580542dedc889259dd6150a6137ba3b
sena57 <126655518+sena57@users.noreply.github.com>
2024-12-16 15:24:53 +0100

    finished 01 Variables in Javascript

---
+++ Javascript/01Variablen/script.js
@@ -0,0 +1,32 @@
+console.log("Hello");
+
+//Variablen
+let a = 7;
+let b = 8;
+let c = 10;
+
+//Berechne folgendes aus und gib das Ergebnis über console aus
+let d = a + b +c
+console.log("a + b + c =" + d);
+
+d = (a + b)/c;
+console.log("(a + b)/c =" + d);
+
+d = c - a + b;
+console.log("c - a + b =" + d);
+
+d = a + b +c * 3;
+console.log("a + b + c * 3 =" + d);
+
+d = (a + b + c)/4
+console.log("(a + b + c)/4 =" + d);
+
+//Ändere die Variablen auf folgende Werte
+a = 3;
+console.log("Die neue Variable für a wäre " + a);
+
+b = a +3;
+console.log("Die neur Variable für b wäre " + b);
+
+c = c - 3;
+console.log("Die neue Variable für c wäre " + c);
