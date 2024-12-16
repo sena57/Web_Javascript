335e03103635e51c39b02e069c1711b073e27461
sena57 <126655518+sena57@users.noreply.github.com>
2024-12-16 15:53:33 +0100

    finished 02Strings JS

--- Javascript/01Variablen/script.js
+++ Javascript/01Variablen/script.js
@@ -6,26 +6,26 @@ let b = 8;
 let c = 10;
 
 //Berechne folgendes aus und gib das Ergebnis über console aus
-let d = a + b +c
+let d = a + b + c
 console.log("a + b + c =" + d);
 
-d = (a + b)/c;
+d = (a + b) / c;
 console.log("(a + b)/c =" + d);
 
 d = c - a + b;
 console.log("c - a + b =" + d);
 
-d = a + b +c * 3;
+d = a + b + c * 3;
 console.log("a + b + c * 3 =" + d);
 
-d = (a + b + c)/4
+d = (a + b + c) / 4
 console.log("(a + b + c)/4 =" + d);
 
 //Ändere die Variablen auf folgende Werte
 a = 3;
 console.log("Die neue Variable für a wäre " + a);
 
-b = a +3;
+b = a + 3;
 console.log("Die neur Variable für b wäre " + b);
 
 c = c - 3;
---
+++ Javascript/02Strings/script.js
@@ -0,0 +1,11 @@
+let a = 17.4;
+let b = 3.9;
+
+result = a / b;
+
+console.log("The result of " + a + " divided by " + b + " is " + result);
+
+let firstname = "Sena";
+let lastname = "Özcan";
+
+console.log("Ich heiße " + firstname +  " " + lastname);
