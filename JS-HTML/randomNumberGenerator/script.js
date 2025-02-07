ef2b8a3b7334043dcea0a16cf4cc8b5ed6b04fa5
sena57 <126655518+sena57@users.noreply.github.com>
2025-02-07 10:29:40 +0100

    finished random number generator

---
+++ JS-HTML/randomNumberGenerator/script.js
@@ -0,0 +1,10 @@
+function generate() {
+    let number1 = document.getElementById("field1").value;
+    let number2 = document.getElementById("field2").value;
+
+    let amountOfRandomNumber = parseInt(number2) - parseInt(number1);
+    console.log(number1 + " : " + number2);
+
+    let randomNumber = Math.floor(Math.random() * amountOfRandomNumber) + parseInt(number1);
+    document.getElementById("randomNumber").innerHTML = randomNumber;
+}
