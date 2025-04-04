9ef7ce4b6e73bda36feab3abf4fd231a438f9154
sena57 <126655518+sena57@users.noreply.github.com>
2025-04-04 10:40:33 +0200

    update styling and remove unnecessary key prop

--- Demo1/src/components/List.jsx
+++ Demo1/src/components/List.jsx
@@ -3,11 +3,11 @@ import React from 'react'
 export default function List() {
   const items = ["Coffee", "Tea", "Beer"];
   return (
-      <div className="max-w-xs mx-auto mt-10">
+      <div className=" ml-10 ">
       <ul className="border border-white">
         {items.map((item, index) => (
           <li
-            key={index}
+            
             className="bg-teal-700 text-white p-4 text-lg border-b border-white hover:bg-teal-600 transition duration-300 cursor-pointer"
           >
             {item}
